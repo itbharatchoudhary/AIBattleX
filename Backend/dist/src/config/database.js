bcaf815d5d045ff0a400e6b7543a6c09cc103e50
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-04-16 13:23:12 +0530

    fix: express wildcard route for production2

--- Backend/dist/src/config/database.js
+++ Backend/dist/src/config/database.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected ✅");
+        console.log("MongoDB Connected ");
     }
     catch (error) {
-        console.error("MongoDB Error ❌", error);
+        console.error("MongoDB Error ", error);
         process.exit(1);
     }
 };
